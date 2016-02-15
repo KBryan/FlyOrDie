70a51b473a495db95205c28c145237a9671b09fd
KBryan <kwame@3einteractive.com>
2016-02-15 10:40:09 -0500

    Added Menu Scene to Game Project

---
+++ FlyOrDie/MenuScene.swift
@@ -0,0 +1,14 @@
+//
+//  MenuScene.swift
+//  FlyOrDie
+//
+//  Created by KBryan on 2016-02-15.
+//  Copyright © 2016 KBryan. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class MenuScene: SKScene {
+
+}
